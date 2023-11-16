b752ec8b9b901a1c528368bfd9138b36382c33e8
Endyfadlullah <endinewjr68@gmail.com>
2023-11-16 20:39:58 +0700

    ubah tampilan login

--- login.php
+++ login.php
@@ -1,30 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>Login V1</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="Login_v1/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="login/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/css/util.css">
-	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/css/util.css">
+	<link rel="stylesheet" type="text/css" href="Login_v1/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -32,67 +26,76 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<div class="login100-form-title" style="background-image: url(login/images/959196.png);">
-					<span class="login100-form-title-1">
-						LOGIN
-					</span>
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="https://img.freepik.com/free-vector/people-computer-tablet-smartphone-design-element_23-2148155585.jpg?w=740&t=st=1700109970~exp=1700110570~hmac=e8abc762ebb68d9305871186dac40be2e35d9723c8664c353d2b52efecdc6d40" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form" action="proses-login.php" method="post">
-					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-						<span class="label-input100">Email</span>
-						<input class="input100" type="email" name="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+					<span class="login100-form-title">
+						Admin Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="email" name="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email">
 						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" id="exampleInputPassword" placeholder="Enter password">
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" id="exampleInputPassword" placeholder="Password">
 						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
 					</div>
-
-					<div class="flex-sb-m w-full p-b-30">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Ingat saya
-							</label>
-						</div>
-
-						<!-- <div>
-							<a href="#" class="txt1">
-								Forgot Password?
-							</a>
-						</div> -->
-					</div>
-
+					
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Login
 						</button>
-						<!-- <input type="submit" name="submit" class="btn btn-primary btn-user btn-block" value="Masuk"> -->
+					</div>
+
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Forgot
+						</span>
+						<a class="txt2" href="#">
+							Username / Password?
+						</a>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="Login_v1/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="login/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="Login_v1/vendor/bootstrap/js/popper.js"></script>
+	<script src="Login_v1/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="login/vendor/animsition/js/animsition.min.js"></script>
+	<script src="Login_v1/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="login/vendor/bootstrap/js/popper.js"></script>
-	<script src="login/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="Login_v1/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
 <!--===============================================================================================-->
-	<script src="login/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/daterangepicker/moment.min.js"></script>
-	<script src="login/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="login/js/main.js"></script>
+	<script src="Login_v1/js/main.js"></script>
 
 </body>
 </html>
