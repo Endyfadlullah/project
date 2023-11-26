c4cbd9fade4be1c8ae6634b6d17750b95257b9ea
Endyfadlullah <endinewjr68@gmail.com>
2023-11-26 10:51:55 +0700

    edit supplier

--- supplier.php
+++ supplier.php
@@ -12,14 +12,14 @@ require 'cek-sesi.php';
   <meta name="description" content="">
   <meta name="author" content="">
   <link rel="shortcut icon" href="img/uang.png">
-  <title>Supplier</title>
+  <title>Data Supplier</title>
 
   <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.css" rel="stylesheet">
 
   <!-- Custom styles for this page -->
   <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
@@ -37,7 +37,7 @@ require 'cek-sesi.php';
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
-        <button type="button" class="btn btn-success" style="margin:5px" data-toggle="modal" data-target="#myModalTambah"><i class="fa fa-plus"> Supplier</i></button><br>
+        <button type="button" class="btn btn-success" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; margin:5px;" data-toggle="modal" data-target="#myModalTambah"><i class="fa fa-plus"></i> Supplier</button><br>
 
 
           <!-- DataTales Example -->
