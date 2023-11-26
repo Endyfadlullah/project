50fe7748f8f17b190dc6c813ab58c60f0760ce81
Endyfadlullah <endinewjr68@gmail.com>
2023-11-26 10:49:39 +0700

    edit karyawam

--- laporan.php
+++ laporan.php
@@ -17,7 +17,7 @@ require 'cek-sesi.php';
   <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <!-- Custom styles for this template -->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.css" rel="stylesheet">
 
   <!-- Custom styles for this page -->
   <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
