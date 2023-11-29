0dd13cb12ba9d03673ab1f99ce23bd1afaeb6c5c
Endyfadlullah <endinewjr68@gmail.com>
2023-11-29 11:14:25 +0700

    ubah font navbar

--- navbar.php
+++ navbar.php
@@ -1,17 +1,20 @@
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+<!-- Topbar -->
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
+  <!-- Sidebar Toggle (Topbar) -->
+  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+    <i class="fa fa-bars"></i>
+  </button>
 
-          <!-- Topbar Search -->
-		<div>
-      <h1>CV.GLOBAL TECHNIC</h1>
-    </div>
+  <!-- Topbar Search -->
+  <div style="font-family: 'Roboto', sans-serif;">
+    <h1>CV.GLOBAL TECHNIC</h1>
+  </div>
 
-<?php require 'user.php'; ?>
+  <?php require 'user.php'; ?>
 
-        </nav>
-        <!-- End of Topbar -->
+</nav>
+<!-- End of Topbar -->
