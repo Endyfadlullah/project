e61078cad1a0636832328c59a4bc1b3e46956ff4
Endyfadlullah <endinewjr68@gmail.com>
2023-12-17 16:49:23 +0700

    p

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require 'cek-sesi.php';
   <meta name="description" content="">
   <meta name="author" content="">
   <link rel="shortcut icon" href="img/uang.png">
-  <title>Keuangan - Admin</title>
+  <title>Keuangan</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
